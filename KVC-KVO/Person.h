1c5fa38f868ffa8820e5f49c69c6fe15b8bc7c8a
王斌 <bin4.wang@changhong.com>
2015-12-10 10:42:02 +0800

    first commit

---
+++ KVC-KVO/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  KVC-KVO
+//
+//  Created by 王斌 on 15/12/10.
+//  Copyright © 2015年 Changhong electric Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ KVC-KVO/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  KVC-KVO
+//
+//  Created by 王斌 on 15/12/10.
+//  Copyright © 2015年 Changhong electric Co., Ltd. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
